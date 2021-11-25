72577e57ea25eba26e5839631771ed75851e3f11
devegoo <57900186+devegoo@users.noreply.github.com>
2021-11-25 10:38:00 +0100

    Add files via upload

---
+++ 2CD_random_color.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+mv *.mp3 ghostcopy/
+for dir in ghostcopy
+do
+    (
+      for i in ghostcopy/*.png
+      do
+      r=$(( $RANDOM % 255 ));
+      g=$(( $RANDOM % 255 ));
+      b=$(( $RANDOM % 255 ));
+          convert "$i" -stroke green -fuzz 15% -strokewidth 1000 -draw 'stroke-linecap round line 500,500 500,500.00001' -colorize $r,$g,$b "$i"
+      done
+    )
+done
